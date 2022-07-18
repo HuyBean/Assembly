89d7e0862583596e2a3ccf87cadea134bd23d4fe
HuyBean <nqhuy0711@gmail.com>
2022-07-18 11:51:14 +0700

    Commit

--- Lab_HTMT/CheckLetter.asm
+++ Lab_HTMT/CheckLetter.asm
@@ -2,12 +2,17 @@
 .model Small
 .stack 100h
 .data 
-MessInput db 10,13,'Nhap ky tu: $'
-MessPrime db 10,13,'Ky tu nay co ma la so nguyen to $'
-MessSquare db 10,13,'Ky tu nay co ma la so chinh phuong $'
-MessEvenLetters db 10,13,'Ky tu nay co ma chua toan chu so chan $'
-MessOddLetters db 10,13,'Ky tu nay co ma chua toan chu so le $'
-Mang db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    MessInput db 10,13,'Nhap ky tu: $'
+    MessOutput db 10,13,'Cac ky tu vua nhap: $'
+    MessPrime db 10,13,'Ky tu co ma la so nguyen to: $'
+    MessSquare db 10,13,'Ky tu co ma la so chinh phuong: $'
+    MessEvenLetters db 10,13,'Ky tu co ma chua toan chu so chan: $'
+    MessOddLetters db 10,13,'Ky tu co ma chua toan chu so le: $'
+    Mang db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    var db 0
+    tmp db 0
+    divvar db 0
+    tmpindex db 0
 .code 
 main proc 
     mov ax, @data
@@ -17,18 +22,25 @@ main proc
     mov dx, offset MessInput
     int 21h
 
-    mov bx, dx
+    mov bx, 0
     mov cx, 10
 Nhapso:
     mov ah, 1
     int 21h
     mov Mang[bx], al
     inc bx
-    loop Nhapso
+    dec cx
+    cmp cx, 0
+    jbe Xuat_so
+    jmp Nhapso
+Xuat_so:
 
     mov cx, 10
     mov bx,0
 
+    mov ah,9
+    mov dx, offset MessOutput
+    int 21h
 Vonglap:
     mov dl, Mang[bx]
     mov ah,2
@@ -36,9 +48,164 @@ Vonglap:
     inc bx
     mov dl, ' '
     int 21h
-    loop Vonglap
+    dec cx
+    cmp cx, 0
+    jbe Tiep
+    jmp Vonglap
+Tiep:
+    mov cx, 10
+    mov di, 0
 
+    mov ah,9
+    mov dx, offset MessPrime
+    int 21h
+Kiemtrasnt:
+    cmp cx, 1
+    jb Tiep_tuc1
+    dec cx
+    mov dl, Mang[di]
+    inc di
+    mov var, dl
+    call CheckSNT
+Tiep_tuc1:
+    mov cx, 10
+    mov di, 0
+
+    mov ah,9
+    mov dx, offset MessSquare
+    int 21h
+KiemtraSCP:
+    cmp cx, 1
+    jb Tiep_tuc2
+    dec cx
+    mov dl, Mang[di]
+    inc di
+    mov var, dl
+    call CheckSquare
+Tiep_tuc2:
+    mov cx, 10
+    mov di, 0
+
+    mov ah,9
+    mov dx, offset MessEvenLetters
+    int 21h
+KiemtraChan:
+    cmp cx, 1
+    jb Tiep_tuc3
+    dec cx
+    mov dl, Mang[di]
+    inc di
+    mov var, dl
+    call CheckEven
+Tiep_tuc3:
+    mov cx, 10
+    mov di, 0
+
+    mov ah,9
+    mov dx, offset MessOddLetters
+    int 21h
+KiemtraLe:
+    cmp cx, 1
+    jb Xong
+    dec cx
+    mov dl, Mang[di]
+    inc di
+    mov var, dl
+    call CheckOdd
+Xong:
     mov ah, 4ch
     int 21h
 main endp
+CheckSNT proc
+    mov tmp, dl
+    cmp tmp,2
+    je IsPrime
+    sub var, 2
+    mov divvar, 2
+ChiaSNT:
+    mov ax, 0
+    mov al, dl
+    div divvar
+    inc divvar
+    cmp ah, 0
+    je HetSnt
+    dec var
+    cmp var, 0
+    jne ChiaSNT
+    jmp IsPrime
+IsPrime:
+    mov ah, 2h
+    mov dl, tmp
+    int 21h
+    mov dl, ' '
+    int 21h
+HetSnt:
+    jmp Kiemtrasnt
+CheckSNT endp
+CheckSquare proc 
+    mov tmp, dl
+Square:
+    mov ax, 0 
+    mov al, tmpindex
+    mul al
+    mov ah, 0
+    cmp al, var
+    ja HetCP
+    cmp al, var
+    je IsSquare
+    inc tmpindex
+    jmp Square
+IsSquare:
+    mov ah, 2h
+    mov dl, tmp
+    int 21h
+    mov dl, ' '
+    int 21h
+HetCP:
+    jmp KiemtraSCP
+CheckSquare endp
+CheckEven proc 
+    mov tmp, dl
+    mov divvar, 10
+ChiaEven:
+    cmp var, 0
+    jbe IsEven
+    mov ax, 0
+    mov al, var
+    div divvar
+    mov var, al
+    test ah, 1
+    jnz HetChan
+    jmp ChiaEven
+IsEven:
+    mov ah, 2h
+    mov dl, tmp
+    int 21h
+    mov dl, ' '
+    int 21h
+HetChan:
+    jmp KiemtraChan
+CheckEven endp
+CheckOdd proc
+    mov tmp, dl
+    mov divvar, 10
+ChiaOdd:
+    cmp var, 0
+    jbe IsOdd
+    mov ax, 0
+    mov al, var
+    div divvar
+    test ah, 1
+    jz HetLe
+    mov var, al
+    jmp ChiaOdd
+IsOdd:
+    mov ah, 2h
+    mov dl, tmp
+    int 21h
+    mov dl, ' '
+    int 21h
+HetLe:
+    jmp KiemtraLe
+CheckOdd endp
 end main
--- Lab_HTMT/TimeSystem.asm
+++ Lab_HTMT/TimeSystem.asm
@@ -21,13 +21,13 @@ main proc
     mov ds, ax
 
     mov ah, 2ch;Thực hiện lấy giờ và phút hệ thống, Giá trị CH = giờ, CL = phút
-    int 21h;Ngắt int 21h, 2ch để thực hiện lấy thời gian hệ thống
+    int 21h;Ngắt int 21h, ah = 2ch để thực hiện lấy thời gian hệ thống
 
     mov HourVar, ch;Gán giá trị giờ vào biến
     mov MinVar, cl;Gán giá trị phút vào biến
 
     mov  ah, 2h;Thực hiện kiểm tra đèn capslock đang bật hay tắt 
-    int  16h; Ngắt int 16h, 2h để lấy thông tin tín hiệu phím đè và gán vào thanh ghi AL
+    int  16h; Ngắt int 16h, ah = 2h để lấy thông tin tín hiệu phím đè và gán vào thanh ghi AL
     ;Test có thể được thay thế bằng AND
     Test al, 40H;Kiểm tra Al có bằng 40H hay không (40H là giá trị của bit chứa phím capslock)
     jz  Tiep_tuc; Nếu flag zero có giá trị nghĩa là capslock đang tắt -> không in lời chào, nhảy đến label tiếp theo
@@ -36,200 +36,207 @@ Tiep_tuc:
     call Hour; Bắt đầu thực hiện việc chuẩn bị các thanh ghi để in giờ hệ thống, gọi hàm
     call Hien_Thi_So_Gio;Gọi hàm hoàn tất việc in giờ hệ thống ra màn hình 
 Phut:
-    call Minute
-    call Hien_Thi_So_Phut
+    call Minute; Bắt đầu thực hiện việc chuẩn bị các thanh ghi để in phút hệ thống, gọi hàm
+    call Hien_Thi_So_Phut;Gọi hàm hoàn tất việc in phút hệ thống ra màn hình 
 Chan_le:
-    Test HourVar,1
-    jnz sole
-    call Gio_Chan
-    jmp Xong
+    Test HourVar,1 ; Kiểm tra xem đây là giờ chẵn hay lẻ bằng việc and với 1 
+    jnz sole ; Nếu flag zero không có giá trị nghĩa là Giờ lẻ và lập tức nhảy đến nhãn sole
+    call Gio_Chan ; Còn lại giờ chẵn sẽ gọi hàm Gio_chan và bắt đầu xử lý thông tin
+    jmp Xong ;lệnh nhảy phòng khi trường hợp hệ thống không nhận ra là chẵn hay lẻ thì sẽ nhảy đễn nhãn xong va kết thúc chương trình
 sole:
-    call Gio_Le
+    call Gio_Le ; Gọi hàm Gio_le và bắt đầu xử lý thông tin
 Xong:
-    mov ah,4ch
-    int 21h
+    mov ah,4ch ; Lệnh trả về kết thúc chương trình
+    int 21h ; Đi với ngắt int 21h
 main endp
+;Chuẩn bị gán các thanh ghi để xử lý thông tin cho việc in giờ
 Hour proc
     mov ah,9
-    mov dx, offset MessHour
+    mov dx, offset MessHour;In dòng chữ hiển thị giờ
     int 21h
 
-    mov dl, HourVar
-    mov ah,0
-    mov al, dl
-    mov dx, 0
-    mov bx,10
-    mov cx,0
+    mov dl, HourVar; Gán giá trị dl = giá trị giờ
+    mov ah,0; xóa các bit cao trong ax
+    mov al, dl; gán tiếp thanh ghi al = giá trị giờ
+    ; Dùng để mov ax, HourVar để thuận lợi cho việc chia
+    mov dx, 0 ;Trả lại giá trị thanh ghi dx = 0
+    mov bx,10; Để bx là biến tạm chứa giá trị 10
+    mov cx,0; Để cx là biến đếm bắt đầu bằng 0
 Hour endp
 
 ;In ra màn hình số giờ, hàm này thực hiện kể cả trong trường hợp giờ có 2 chữ số
 Hien_Thi_So_Gio PROC
 chiaGio: 
-    div bx ;lây kêt qua chia cho 10
-    push dx ;du o dx đây vao ngăn xêp
-    inc cx ;tăng biên đêm
-    cmp ax,0 ;so sanh thuong voi 0
-    je hienkqGio ;neu băng thì hiên kêt qua
-    mov dx,0 ;xoa bit cao trong dx
+    div bx ;lấy kêt quả trong ax chia cho 10
+    push dx ;Lấy kết quả phép chia dư bỏ vào trong ngăn xếp dx
+    inc cx ;tăng biến đếm để biết số ký tự
+    cmp ax,0 ;Nếu thương bằng 0
+    je hienkqGio ;Thì dừng việc chia lấy ký tự và nhảy đến nhãn hienkqGio
+    mov dx,0 ;xóa bit cao trong dx để thực hiện tiếp việc lấy ký tự
     jmp chiaGio
 hienkqGio: 
 ;He thong se pop va in ra man hinh den khi so vong lap bang cx
-    pop dx ;lây du trong ngăn xêp ra khoi dx
-    add dl,30h ;chuyên sô thành dang ký tu
-    mov ah,2 ;in tông
+    pop dx ;lây dư trong kết quả ở bước trên ra dx
+    add dl,30h ;chuyển số thành dạng ký tự để chuẩn bị in ra man hình
+    mov ah,2 ;in ký tự bằng lệnh gán ah = 2
     int 21h
-    dec cx
-    cmp cx,0
-    ja hienkqGio
+    dec cx ;Giảm dần biến đếm
+    cmp cx,0 ; Nếu đếm xong thì kết thúc 
+    ja hienkqGio;Nếu biến đếm vẫn còn thì in tiếp ký tự 
 Hien_Thi_So_Gio ENDP
+;Chuẩn bị gán các thanh ghi để xử lý thông tin cho việc in phút
 Minute proc
 
     mov ah,9
-    mov dx, offset MessMin
+    mov dx, offset MessMin;In dòng chữ hiển thị phút
     int 21h
-    
+    ; Gán mov ax, MinVar để thuận lợi cho việc chia
     mov dl, MinVar
     mov ah,0
     mov al, dl
+    ; Trả lại giá trị các thanh ghi 
     mov dx, 0
-    mov bx,10
-    mov cx,0
+    mov bx,10;Biến tạm là bx chứa giá trị 10
+    mov cx,0; Biến đếm là cx bắt đâu bằng 0
 Minute endp
 ;In ra màn hình số phút, hàm này thực hiện kể cả trong trường hợp phút có 2 chữ số
 Hien_Thi_So_Phut PROC
 chiaPhut: 
-    div bx ;lây kêt qua chia cho 10
-    push dx ;du o dx đây vao ngăn xêp
-    inc cx ;tăng biên đêm
-    cmp ax,0 ;so sanh thuong voi 0
-    je hienkqPhut ;neu băng thì hiên kêt qua
-    mov dx,0 ;xoa bit cao trong dx
-    jmp chiaPhut
+    div bx ;lấy kết quả trong ax chi cho 10
+    push dx ;kết quả dư của phép chia đẩy vào ngăn xếp dx
+    inc cx ;tăng biến đếm
+    cmp ax,0 ;so sánh kết quả phép chia với 0
+    je hienkqPhut ;nếu bằng thì dừng và nhảy đến bước in phút
+    mov dx,0 ;xóa bit cao trong dx để tiếp tục đẩy vào ký tự
+    jmp chiaPhut ;nhảy đến nhãn chiaPhut nếu phép chia chưa hết
 hienkqPhut: 
-;He thong se pop va in ra man hinh den khi so vong lap bang cx
-    pop dx ;lây du trong ngăn xêp ra khoi dx
-    add dl,30h ;chuyên sô thành dang ký tu
-    mov ah,2 ;in tông
+;Hệ thống sẽ pop và in ra màn hình đến khi số vòng lặp bằng cx
+    pop dx ;lấy ký tự dần dần ra khỏi ngăn xếp dx
+    add dl,30h ;chuyển số thành ký tự để in ra màn hình
+    mov ah,2 ;in ký tự
     int 21h
-    dec cx
-    cmp cx,0
-    ja hienkqPhut
-    jmp Chan_le
-
+    dec cx ;giảm dần biến đếm
+    cmp cx,0; so sánh biến đếm 
+    ja hienkqPhut; nếu chưa bằng 0 thì tiếp tục đẩy ra và in
+    jmp Chan_le; nếu bằng 0 nghĩa là vòng lặp hết và kết thúc
 Hien_Thi_So_Phut ENDP
-
+;Hàm hiển thị các lời chào theo giờ tương ứng
 Loi_Chao proc 
-    cmp HourVar,12
-    jae Trua
+    cmp HourVar,12 ;So sánh giờ với 12
+    jae Trua; nếu lớn hơn hoặc bằng thì nhảy đến nhãn Trua
     mov ah,9h
-    mov dx, offset MessMorning
+    mov dx, offset MessMorning; Nếu nhỏ hơn thì in dòng chào buổi sáng
     int 21h
-    jmp Tiep_tuc
+    jmp Tiep_tuc; Nhảy đến nhãn Tiep_tuc trên hàm main
 Trua:
-    cmp HourVar,18
-    jae Chieu
+    cmp HourVar,18 ; So sánh giờ với 18
+    jae Chieu ; Nếu lớn hơn hoặc bằng thì nhảy đến nhãn Chieu
     mov ah,9h
-    mov dx, offset MessAfternoon
+    mov dx, offset MessAfternoon;Nếu nhỏ hơn thì in dòng chào buổi trưa
     int 21h
-    jmp Tiep_tuc
+    jmp Tiep_tuc;Nhảy đến nhãn Tiep_tuc trên hàm main
 Chieu:
     mov ah,9h
-    mov dx, offset MessEvening
+    mov dx, offset MessEvening;Nếu không phải buổi trưa và sáng thì in dòng chào buổi chiều
     int 21h
-    jmp Tiep_tuc
+    jmp Tiep_tuc;Nhảy đến nhãn Tiep_tuc trên hàm main
 Loi_Chao endp
-
+;Hàm nếu là giờ chẵn thì xử lý thông tin theo yêu cầu đề bài
 Gio_Chan proc 
     mov ah,9
-    mov dx, offset MessEven
+    mov dx, offset MessEven ;In dòng "Assembly language"
     int 21h
-    jmp Xong
+    jmp Xong;Nhảy đến nhãn Xong trên hàm main
 Gio_Chan endp
-
+;Hàm nếu là giờ lẻ thì xử lý thông tin theo yêu cầu đề bài
 Gio_Le proc 
     mov ah,9h
-    mov dx, offset MessSum
+    mov dx, offset MessSum; In dòng "H + M = "
     int 21h
-    inc TempVar
-    mov al, HourVar
-    add al, MinVar
-    mov dl, al
+    inc TempVar;Tăng biến đếm để đảm bảo không lặp lại bước này
+    mov al, HourVar; Gán al = HourVar
+    add al, MinVar; Cộng thêm Min Var
+    mov dl, al; Gán cho dl để tiện cho việc in kết quả
+    ;Chuẩn bị giá trị các thanh ghi để in kết quả
     mov ah,0
     mov al, dl
     mov dx, 0
-    mov bx,10
-    mov cx,0
+    mov bx,10;Biến tạm bx giữ giá trị 10
+    mov cx,0;Biến đếm là cx bắt đầu từ 0
 chia_ket_qua: 
-    div bx ;lây kêt qua chia cho 10
-    push dx ;du o dx đây vao ngăn xêp
-    inc cx ;tăng biên đêm
-    cmp ax,0 ;so sanh thuong voi 0
-    je hien_kq ;neu băng thì hiên kêt qua
-    mov dx,0 ;xoa bit cao trong dx
-    jmp chia_ket_qua
+    div bx ;lấy kết quả trng ax chia cho 10
+    push dx ;dư đẩy vào ngăn xếp
+    inc cx ;tăng biến đếm
+    cmp ax,0 ;so sánh ax với 0
+    je hien_kq ;nếu bằng thì hiện kết quả
+    mov dx,0 ;xóa bit cao trong dx để lấy tiếp ký tự
+    jmp chia_ket_qua; Nhảy đến nhãn chia_ket_qua
 hien_kq: 
-;He thong se pop va in ra man hinh den khi so vong lap bang cx
-    pop dx ;lây du trong ngăn xêp ra khoi dx
-    add dl,30h ;chuyên sô thành dang ký tu
-    mov ah,2 ;in tông
+;Hệ thống sẽ pop và in ra màn hình đến khi số vòng lặp bằng cx
+    pop dx ;lấy dư trong ngăn xếp ra khỏi dx để in ra màn hình
+    add dl,30h ;chuyển số thành dạng ký tự
+    mov ah,2 ;in ký tự
     int 21h
-    dec cx
-    cmp cx,0
-    ja hien_kq
+    dec cx; giảm biến đếm 
+    cmp cx,0; so sánh biến đếm 
+    ja hien_kq; nếu chưa bằng 0 thì tiếp tục đẩy ra và in
 
-    cmp TempVar, 3
-    je Het
-    cmp TempVar, 2
-    je Chia
-    jmp tru
+    cmp TempVar, 3; so sánh biến đếm với 3
+    je Het; nếu đủ 3 yêu cầu thì nhảy đến nhãn Het
+    cmp TempVar, 2 ;so sánh biến đếm với 2
+    je Chia ; nếu chỉ đủ 2 yêu cầu thì nhảy đến nhãn chia để hoàn thành yêu cầu còn lại 
+    jmp tru; còn lại nếu chỉ mới 1 yêu cầu thì tiếp tục phía dưới
 Tru: 
     mov ah,9h
-    mov dx, offset MessSub
+    mov dx, offset MessSub;In dòng "H - M ="
     int 21h
-    mov al, HourVar
-    cmp al, MinVar
-    jb So_am
-    sub al, MinVar
-    inc TempVar
+    mov al, HourVar;gán al = HourVar
+    cmp al, MinVar;so sánh giờ với phút 
+    jb So_am; nếu giờ < phút thì kết quả sẽ ra số âm vì vậy nhảy đến nhãn So_am để xử lý 
+    sub al, MinVar; nếu giờ > phút thì lấy hiệu như bình thường
+    inc TempVar;Tăng biến đếm để đảm bảo không lặp lại bước này
+    ;Chuẩn bị các giá trị thanh ghi để bắt đầu việc in ra màn hình kết quả phép trừ
     mov dl, al
     mov ah,0
     mov al, dl
     mov dx, 0
-    mov bx,10
-    mov cx,0
-    jmp chia_ket_qua
+    mov bx,10;Biến tạm là bx giữ giá trị 10
+    mov cx,0;Biến đém là cx bắt đầu đếm từ 0
+    jmp chia_ket_qua;Nhảy đến nhãn chia_ket_qua để in kết quả ra màn hình
 So_am:
     mov ah,9h
-    mov dx, offset MessNegative
+    mov dx, offset MessNegative ;in ra ký tự "-" để nhận biết số âm
     int 21h
 
-    mov al, MinVar
-    sub al, HourVar
-    inc TempVar
+    mov al, MinVar; gán al = MinVar trước
+    sub al, HourVar; rồi trừ cho HourVar
+    inc TempVar;Tăng biến đếm để đảm bảo không lặp lại bước này
+    ;Chuẩn bị các giá trị thanh ghi để bắt đầu việc in ra màn hình kết quả phép trừ
     mov dl, al
     mov ah,0
     mov al, dl
     mov dx, 0
-    mov bx,10
-    mov cx,0
-    jmp chia_ket_qua
+    mov bx,10;Biến tạm là bx giữ giá trị 10
+    mov cx,0;Biến đém là cx bắt đầu đếm từ 0
+    jmp chia_ket_qua;Nhảy đến nhãn chia_ket_qua để in kết quả ra màn hình
 Chia:
     mov ah,9h
-    mov dx, offset MessMod
+    mov dx, offset MessMod;in dòng 'H % M = '
     int 21h
-    mov al, HourVar
-    mov ah, 0
-    mov bl, MinVar
-    div bl
-    mov dl, ah
-    inc TempVar
+    mov al, HourVar ;gán al = HourVar
+    mov ah, 0; dùng để cho ax có giá trị = al 
+    mov bl, MinVar; gán bl = MinVar
+    div bl; Phép div nghĩa là lấy ax chia bl và kết quả phép dư lưu vào ah, thương lưu vào al
+    mov dl, ah; gán phép dư cho dl để tiện cho việc in
+    inc TempVar;Tăng biến đếm để đảm bảo không lặp lại bước này
+    ;Chuẩn bị các giá trị thanh ghi để bắt đầu việc in ra màn hình kết quả phép trừ
     mov ah,0
     mov al, dl
     mov dx, 0
-    mov bx,10
-    mov cx,0
-    jmp chia_ket_qua
+    mov bx,10;Biến tạm là bx giữ giá trị 10
+    mov cx,0;Biến đém là cx bắt đầu đếm từ 0
+    jmp chia_ket_qua;Nhảy đến nhãn chia_ket_qua để in kết quả ra màn hình
 Het:
-    jmp Xong
+    jmp Xong;Nhảy đến nhãn Xong trong hàm main và hoàn tất chương trình
 Gio_Le endp
 end main
